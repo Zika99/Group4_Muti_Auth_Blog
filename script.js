81d3638d4877f81ca85061ca70285f60e88f8116
demi05 <demiladeleshi@gmail.com>
2022-10-12 14:16:09 +0100

    mobile commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const hamBurger = document.querySelector(".fa-bars");
+const navList = document.querySelector(".nav-list");
+
+hamBurger.addEventListener("click",()=>{
+    navList.classList.toggle("active");
+})
