67dfbcd3e56cda410efb89d37cd32e2ac36f296b
demi05 <demiladeleshi@gmail.com>
2022-10-13 18:14:15 +0100

    commit now

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 
 const hamBurger = document.querySelector(".fa-bars");
 const navList = document.querySelector(".nav-list");
+// const leftUl = document.querySelector(".left")
 
 hamBurger.addEventListener("click",()=>{
     navList.classList.toggle("active");
 })
+
+// hamBurger.addEventListener("click",()=>{
+//     leftUl.classList.toggle("active");
+// })
